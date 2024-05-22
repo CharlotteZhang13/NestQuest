1675c0a996b1c466069331800f6dcbdd0b541b50
CharlotteZhang13 <104126300+CharlotteZhang13@users.noreply.github.com>
2024-05-22 08:57:34 +0800

    other dependencies

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+const {Server} = require("socket.io");
+const io = new Server(server);
+
+const rooms = {};
+
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/healthcheck', (req, res) =>{
+    res.send('<h1>RPS App running...</h1>');
+})
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/client/index.html');
+})
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+})
