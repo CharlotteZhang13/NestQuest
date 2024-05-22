96af089627e7206e4165a34e0568db306c85796b
CharlotteZhang13 <104126300+CharlotteZhang13@users.noreply.github.com>
2024-05-22 11:25:42 +0800

    co-editing try

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const server = http.createServer(app);
 const {Server} = require("socket.io");
 const io = new Server(server);
 
+const rooms = {};
+
 app.use(express.static(__dirname));
 
 app.get('/healthcheck', (req, res) =>{
@@ -21,9 +23,38 @@ io.on('connection', (socket) =>{
         console.log("user disconnected");
     })
     
-    socket.on("")
+    socket.on("createEditor", () => {
+        const roomUniqueId = makeid(6);
+        socket.join(roomUniqueId);
+        rooms[roomUniqueId] = {};
+        socket.emit("newEditor", {roomUniqueId: roomUniqueId})
+    })
+
+    socket.on('joinEditor', (data) => {
+        if(rooms[data.roomUniqueId] != null) {
+            socket.join(data.roomUniqueId);
+            socket.emit("newEditor", {roomUniqueId: data.roomUniqueId})
+        }
+    })
+
+    socket.on("updateVar", (data) => {
+        console.log(data.newVar);
+        io.to(data.roomUniqueId).emit("receiveVar", {
+            newVar: data.newVar
+        });
+    })
 })
 
 server.listen(3010, () => {
     console.log('listening on *:3010');
 })
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
